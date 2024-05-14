3a09046947e67ff1b926bc32d2751a87004823f9
Taradan2007 <165767895+Taradan2007@users.noreply.github.com>
2024-05-14 10:07:58 +0200

    tacosd

--- fantasmas.gd
+++ fantasmas.gd
@@ -6,21 +6,17 @@ var player = null
 
 func _physics_process(delta):
 	if player_chase:
-		velocity = (player.get_global_position() - position).normalized() * speed * delta
+		velocity = (player.get_global_position() - global_position).normalized() * speed * delta
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, 0.07)
 	move_and_collide(velocity)
 		
-		
-		
-		
-		
-		
+
+
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
 	
-
 
 func _on_detection_area_body_exited(body):
 	player = null
